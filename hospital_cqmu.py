79bbf44f947d076fd23be8c073bcb86ffe79f4bd
Anqi Guo <guo.anqi@icloud.com>
2025-01-15 15:39:05 +0800

    create Hospital class and initialize parameters

--- hospital_cqmu.py
+++ hospital_cqmu.py
@@ -1,6 +1,18 @@
 from dotenv import load_dotenv
 import os
+import ast
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
 
 load_dotenv()
 DEPARTMENT = os.getenv("DEPARTMENT")
 DOCTOR = os.getenv("DOCTOR")
+CAPS = ast.literal_eval(os.getenv('CAPS'))
+
+class Hospital:
+    def __init__(self):
+        self.driver = webdriver.Remote(
+            'http://localhost:4723',
+            options=UiAutomator2Options().load_capabilities(CAPS)
+        )
+        self.size = self.driver.get_window_size()
