38409f7577b12ce91b2e6e9229b0e9b478a7d0bb
Anqi Guo <guo.anqi@icloud.com>
2025-01-15 16:05:54 +0800

    enter program

--- hospital_cqmu.py
+++ hospital_cqmu.py
@@ -3,6 +3,7 @@ import os
 import ast
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
+from selenium.webdriver.common.by import By
 
 load_dotenv()
 DEPARTMENT = os.getenv("DEPARTMENT")
@@ -15,8 +16,20 @@ class Hospital:
             'http://localhost:4723',
             options=UiAutomator2Options().load_capabilities(CAPS)
         )
+        # device window size
         self.size = self.driver.get_window_size()
+    def enter_program(self):
+        # swipe
+        self.driver.flick(
+            self.size['width'] * 0.5,
+            self.size['height'] * 0.4,
+            self.size['width'] * 0.5,
+            self.size['height'] * 0.9
+        )
+        # click on hospital icon
+        self.driver.find_element(By.XPATH, '//*[@text="重医一院"]').click()
 
 if __name__ == "__main__":
     hospital = Hospital()
+    hospital.enter_program()
     print(1)
