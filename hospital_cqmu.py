bfc047cc4320912e6bf11de8b5e37c6e71433f99
Anqi Guo <guo.anqi@icloud.com>
2025-01-15 16:24:57 +0800

    from homepage to department page

--- hospital_cqmu.py
+++ hospital_cqmu.py
@@ -4,8 +4,12 @@ import ast
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 load_dotenv()
+HOSPITAL = os.getenv("HOSPITAL")
 DEPARTMENT = os.getenv("DEPARTMENT")
 DOCTOR = os.getenv("DOCTOR")
 CAPS = ast.literal_eval(os.getenv('CAPS'))
@@ -18,18 +22,20 @@ class Hospital:
         )
         # device window size
         self.size = self.driver.get_window_size()
-    def enter_program(self):
-        # swipe
-        self.driver.flick(
-            self.size['width'] * 0.5,
-            self.size['height'] * 0.4,
-            self.size['width'] * 0.5,
-            self.size['height'] * 0.9
-        )
-        # click on hospital icon
-        self.driver.find_element(By.XPATH, '//*[@text="重医一院"]').click()
+
+    def to_hospital(self):
+        # click 门诊挂号
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@text="门诊挂号"]')))
+        self.driver.find_element(By.XPATH, '//*[@text="门诊挂号"]').click()
+        # click 院区
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@text="本部院区"]')))
+        self.driver.find_element(By.XPATH, f'//*[@text="{HOSPITAL}"]').click()
+        # click 确定
+        time.sleep(6)
+        self.driver.find_element(By.XPATH, '//*[contains(@text,"阅读并同意挂号预约须知")]').click()
+        self.driver.find_element(By.XPATH, '//*[@text="确定"]').click()
 
 if __name__ == "__main__":
     hospital = Hospital()
-    hospital.enter_program()
+    hospital.to_hospital()
     print(1)
