ebfb56501e939cd153f68b005e3733a994cbc917
Anqi Guo <guo.anqi@icloud.com>
2025-01-18 23:09:55 +0800

    moved out the exception handling work out of the run() method and added comments

--- HospitalMiniProgram.py
+++ HospitalMiniProgram.py
@@ -29,36 +29,51 @@ class HospitalMiniProgram(BaseAutomation):
 
     def run(self):
         try:
+            # ensure the mini program is open
             if not self.homepage.is_open():
                 self.homepage.open_mini_program()
 
+            # start outpatient registration
             self.homepage.outpatient_registration()
             self.restart_cnt = 0
+
+            # handle login if required
             if self.login_page.is_login_required():
                 self.login_page.login()
+
+            # select branch
             self.booking.select_branch(self.branch)
+
+            # attempt booking
             while True:
+                # select department
                 self.booking.select_department(self.department, self.subdepartment)
+                # select doctor
                 self.booking.select_doctor(self.doctor)
+                # select time
                 if self.booking.select_time():
-                    break
+                    break # if there are available slot, then quit the while loop
                 else:
-                    self.retry_search()
-        except RecursionError as e:
-            logging.error("Reached recursion limit")
-            self.quit_program()
-        except NoSuchElementException as e:
-            logging.error(f"Element not found")
-        except TimeoutException as e:
-            logging.error(f"Operation time out")
-        except WebDriverException:
-            logging.error("Web driver issue")
+                    self.retry_search() # go back to the select department page and retry search
+        except (RecursionError, NoSuchElementException, TimeoutException, WebDriverException) as e:
+            self.handle_error(e)
         except Exception as e:
-            logging.error(f"Workflow failed: {e}")
-            if self.restart_cnt < 4:
-                self.restart_program()
-            else:
-                self.clear_cache()
+            logging.error(f"Workflow failed with unexpected error: {e}")
+            self.handle_error(e)
+
+    def handle_error(self, e):
+        errors = {
+            RecursionError: 'Reached recursion limit',
+            NoSuchElementException: 'Element not found',
+            TimeoutException: 'Operation time out',
+            WebDriverException: 'Web driver issue'
+        }
+        logging.error(errors[e])
+        if self.restart_cnt < 4:
+            self.restart_cnt += 1
+            self.restart_program()
+        else:
+            self.clear_cache()
 
     def restart_program(self):
         logging.info("Restarting mini program...")
