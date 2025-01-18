aa9279d3b426f24c32427f2a44de286ad0c7bd84
Anqi Guo <guo.anqi@icloud.com>
2025-01-18 14:21:28 +0800

    check if a window pops up, and switch context on this page

--- Hompage.py
+++ Hompage.py
@@ -1,15 +1,23 @@
 from BaseAutomation import BaseAutomation
 import time
+from selenium.webdriver.common.by import By
+
 
 class Homepage(BaseAutomation):
     def __init__(self, driver):
         super().__init__(driver)
 
     def outpatient_registration(self):
+        # 弹窗：同意
         try:
-            self.click_element("//*[@text='门诊挂号']")
-            time.sleep(1)
-            self.driver.switch_to.context("WEBVIEW_com.tencent.mm:appbrand0")
+            self.driver.find_element(By.XPATH, '//android.widget.Button[@text=" 同意 "]').click()
+        except Exception:
+            pass
+
+        try:
+            self.driver.switch_to.context('WEBVIEW_com.tencent.mm:appbrand2')
+            self.switch_window("index")
+            self.click_element("//wx-view[@class='title' and text()='门诊挂号']")
         except Exception as e:
             print(f"Error on homepage: {e}")
             raise
