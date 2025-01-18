1f50b5c6c77436294a71307974c437f2ea36a9d0
Anqi Guo <guo.anqi@icloud.com>
2025-01-18 15:59:21 +0800

    try different contexts to switch

--- Hompage.py
+++ Hompage.py
@@ -10,13 +10,14 @@ class Homepage(BaseAutomation):
     def outpatient_registration(self):
         # 弹窗：同意
         try:
-            self.driver.find_element(By.XPATH, '//android.widget.Button[@text=" 同意 "]').click()
+            self.click_element('//android.widget.Button[@text=" 同意 "]')
         except Exception:
             pass
-
         try:
-            self.driver.switch_to.context('WEBVIEW_com.tencent.mm:appbrand2')
-            self.switch_window("index")
+            self.driver.switch_to.context('WEBVIEW_com.tencent.mm:appbrand0') # 有时候要切换到appbrand2
+            if not self.switch_window("index"):
+                self.driver.switch_to.context('WEBVIEW_com.tencent.mm:appbrand2')
+                self.switch_window("index")
             self.click_element("//wx-view[@class='title' and text()='门诊挂号']")
         except Exception as e:
             print(f"Error on homepage: {e}")
